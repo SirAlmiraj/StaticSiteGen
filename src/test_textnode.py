a3a2ce648f34315af27080c6fba698766675b542
SirAlmiraj <danialomar24@gmail.com>
2026-06-13 11:45:20 +0100

    text to html function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -13,9 +13,21 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_urlne(self):
-        node = TextNode("The URL man", TextType.PLAIN, "https://something.com")
-        node2 = TextNode("The URL man", TextType.PLAIN)
+        node = TextNode("The URL man", TextType.BOLD, "https://something.com")
+        node2 = TextNode("The URL man", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_italic(self):
+        node = TextNode("Italian text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italian text")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 from enum import Enum 
+from HTMLNode import LeafNode
 
 class TextType(Enum):
-    PLAIN = "plain"
-    BOLD = "**Boldtext**"
-    ITALIC = "_Italic_"
-    CODE = "`Code`"
-    LINKS = "[anchor](url)"
-    IMAGES = "![alt](url)"
+    TEXT = "" #"plain"
+    BOLD = "b" #"**Boldtext**"
+    ITALIC = "i" #"_Italic_"
+    CODE = "`" #"`Code`"
+    LINKS = "a" #"[anchor](url)"
+    IMAGES = "img" #"![alt](url)"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -19,6 +20,26 @@ class TextNode:
             return True
         return False
 
-
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+#Changing a text node into a html LeafNode
+def text_node_to_html_node(text_node: TextNode)->LeafNode:
+    # if the text node is an empty/ None node raise exception
+    if text_node.text_type == None:
+        raise Exception("The node doesn't exist")
+    
+    # match text type and return the right leaf node class
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None ,text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode(TextType.BOLD.value, text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode(TextType.ITALIC.value, text_node.text, None)
+        case TextType.CODE:
+            return LeafNode(TextType.CODE.value, text_node.text, None)
+        case TextType.LINKS:
+            return LeafNode(TextType.LINKS.value, text_node.text, self.url)
+        case TextType.IMAGES:
+            return LeafNode(TextType.IMAGES.value, "", self.url)
